2725eb95f297503c58fe2946053301a0fbb7fb06
nandhini99-club <nandy99200@gmail.com>
2025-04-19 14:10:11 +0530

    Create Database connection.java

---
+++ Database connection.java
@@ -0,0 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/your_database_name";
+    private static final String USER = "your_mysql_username";
+    private static final String PASSWORD = "your_mysql_password";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Connected to MySQL database!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("Connection error: " + e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        getConnection();
+    }
+}
